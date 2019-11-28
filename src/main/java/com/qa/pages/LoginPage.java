0622170dbb93ee939808f86a4fc1a6a48f048d48
swati0205 <swati0205.in@gmail.com>
2019-11-27 20:30:20 -0500

    added Searchpage feature code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -2,4 +2,15 @@ package com.qa.pages;
 
 public class LoginPage {
 
+	
+	public void loginPage()
+	{
+		System.out.println("loginPage");
+	}
+	
+	public void loginSet()
+
+	{
+		System.out.println("loginSet");
+	}
 }
