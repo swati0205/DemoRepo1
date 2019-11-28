60846373662cb69701a9b18210adf83e45a36a05
swati0205 <swati0205.in@gmail.com>
2019-11-27 19:29:15 -0500

    first git commit for DemoRepo1

---
+++ src/main/java/com/qa/pages/HomePage.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class HomePage {
+
+	public void homepage()
+	{
+		System.out.println("homepage");
+	}
+}
---
+++ src/test/java/com/qa/test/LoginPageTest.java
@@ -0,0 +1,13 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+
+	@Test
+	public void test1()
+	{
+		System.out.println("test1");
+	}
+	
+}
